4878202b82195b319b7caa5e22d4a90aa567e03b
natalia <seraya.93@inbox.ru>
2022-05-01 22:23:41 +0300

    wip registration form

--- src/components/tutors/registration-form.vue
+++ src/components/tutors/registration-form.vue
@@ -4,25 +4,45 @@
     <form @submit.prevent="register">
       <div class="flex flex-col space-y-8">
         <!-- FIRSTNAME -->
-        <label class="flex flex-col space-y-2">
-          <span class="font-bold">First name</span>
-          <input type="text" class="ts-input" v-model.trim="tutor.firstName" />
-        </label>
+          <ts-field-input
+            v-model="tutor.firstName.value"
+            v-model:valid="tutor.firstName.valid"
+            v-model:touched="tutor.firstName.touched"
+            required
+          >
+            First name
+          </ts-field-input>
+
         <!-- LASTNAME -->
-        <label class="flex flex-col space-y-2">
-          <span class="font-bold">Last name</span>
-          <input type="text" class="ts-input"  v-model.trim="tutor.lastName" />
-        </label>
+        <ts-field-input
+          v-model="tutor.lastName.value"
+          v-model:valid="tutor.lastName.valid"
+          v-model:touched="tutor.firstName.touched"
+          required
+        >
+          Last name
+        </ts-field-input>
+
         <!-- DESCRIPTION -->
-        <label class="flex flex-col space-y-2">
-          <span class="font-bold">Description</span>
-          <textarea rows="5" class="ts-input pt-2" v-model="tutor.description"/>
-        </label>
+        <ts-field-textarea
+          v-model="tutor.description.value"
+          v-model:valid="tutor.description.valid"
+          v-model:touched="tutor.firstName.touched"
+          required
+        >
+          Description
+        </ts-field-textarea>
+
         <!-- HOURLY RATE -->
-        <label class="flex flex-col space-y-2">
-          <span class="font-bold">Hourly rate</span>
-          <input type="number" class="ts-input" v-model.number="tutor.hourlyRate" />
-        </label>
+        <ts-field-input
+          v-model="tutor.hourlyRate.value"
+          v-model:valid="tutor.hourlyRate.valid"
+          v-model:touched="tutor.firstName.touched"
+          required
+        >
+          Hourly rate
+        </ts-field-input>
+
         <!-- AREAS OF EXPERTISE -->
         <div class="flex flex-col space-y-2">
           <span class="font-bold">Areas of expertise</span>
@@ -37,36 +57,71 @@
 <script>
 import { defineAsyncComponent } from 'vue';
 import { AREAS_OPTIONS } from '~/constants.js'
+import clonedeep from 'lodash.clonedeep'
 
 const REGISTRATION_MODEL = {
-  firstName: '',
-  lastName: '',
-  description: '',
-  hourlyRate: null
+  firstName: {
+    value: '',
+    valid: true,
+    touched: false
+  },
+  lastName: {
+    value: '',
+    valid: true,
+    touched: false
+  },
+  description: {
+    value: '',
+    valid: true,
+    touched: false
+  },
+  hourlyRate: {
+    value: '',
+    valid: true,
+    touched: false
+  }
 }
 
 export default {
   name: 'registration-form',
   components: {
+    TsFieldInput: defineAsyncComponent(() =>
+      import('~/components/fields/ts-field-input.vue')
+    ),
+    TsFieldTextarea: defineAsyncComponent(() =>
+      import('~/components/fields/ts-field-textarea.vue')
+    ),
     TsFieldChecklist: defineAsyncComponent(() =>
       import('~/components/fields/ts-field-checklist.vue')
     )
   },
   emits: ['register'],
   data: () => ({
-    tutor: REGISTRATION_MODEL,
+    tutor: clonedeep(REGISTRATION_MODEL),
     areasOptions: AREAS_OPTIONS,
     checkedAreas: []
   }),
   methods: {
     register() {
+      const isTouched= Object.keys(this.tutor).every(item => item.touched)
+      if(!isTouched) {
+        return
+      }
+
+      const isValid = Object.keys(this.tutor).every(item => item.valid)
+      if(!isValid) {
+        return
+      }
+
+      console.log('submitted')
+
       const data = {
         id: '818690e3-d1d9-489e-82cc-799e640d16fb',
-        firstName: this.tutor.firstName,
-        lastName: this.tutor.lastName,
-        areas: this.checkedAreas,
-        description: this.tutor.description,
-        hourlyRate: this.tutor.hourlyRate
+        firstName: this.tutor.firstName.value,
+        lastName: this.tutor.lastName.value,
+        areas: this.checkedAreas.value,
+        description: this.tutor.description.value,
+        hourlyRate: this.tutor.hourlyRate.value
       }
 
       this.$emit('register', data)
