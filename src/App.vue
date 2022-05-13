a6a119ee48b2f61ca4dee4ab4199c59bb14663a6
Natalya Semka <seraya.93@inbox.ru>
2022-05-13 21:26:34 +0300

    fix route transition

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <template>
-    <ts-header />
-    <Transition name="route">
-      <router-view />
+  <ts-header />
+  <router-view v-slot="{ Component }">
+    <Transition name="route" mode="out-in">
+      <component :is="Component" />
     </Transition>
+  </router-view>
 </template>
 
 <script>
